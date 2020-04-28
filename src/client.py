23ea2aa7420f90e571dcbbfbd11e4770e9fd4f7f
devansh1305 <dpanirwa@purdue.edu>
2020-04-28 02:21:29 -0400

    using display port

--- src/client.py
+++ src/client.py
@@ -1,41 +1,48 @@
+import os
+from multiprocessing import Process
 import cv2
 import pickle
 import zlib
 from socket import *
 
-# create client socket
-client_socket = socket(AF_INET, SOCK_STREAM)
-# connect client socket to localhost with specified port
-client_socket.connect(("127.0.0.1", 12345))
-# name a window and adjust it's size
-cv2.namedWindow("screen", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("screen", 1440, 900)
-try:
-    # continuous loop that gets frames from the server
-    while True:
-        try:
-            num = "" # used to get the number of frame bytes to read from the server
-            data = client_socket.recv(1).decode()
-            dump = b''
-            while data != "\n": # continue reading the frame size until new line
-                num += data
+
+if __name__=="__main__":
+
+    display_stream = os.environ['DISPLAY']
+
+    # create client socket
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    # connect client socket to localhost with specified port
+    client_socket.connect(("127.0.0.1", 12345))
+    # name a window and adjust it's size
+    cv2.namedWindow("screen", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("screen", 1440, 900)
+    try:
+        # continuous loop that gets frames from the server
+        while True:
+            try:
+                num = "" # used to get the number of frame bytes to read from the server
                 data = client_socket.recv(1).decode()
-            # convert frame size num into an int
-            num = int(num)
-            # begin reading the frame from the server
-            while num > 0: 
-                dump += client_socket.recv(min(100000, num))
-                num -= min(100000, num)
-            # deserialize the frame
-            frame = pickle.loads(zlib.decompress(dump))
-            # display the frame
-            cv2.imshow("screen", frame)
-            # checks if ESC is pressed. If so, then the screen sharing window will close
-            if cv2.waitKey(1) == 27:
-                break
-        except:
-            continue
-except KeyboardInterrupt:
-    pass
-# close connection
-client_socket.close()
+                dump = b''
+                while data != "\n": # continue reading the frame size until new line
+                    num += data
+                    data = client_socket.recv(1).decode()
+                # convert frame size num into an int
+                num = int(num)
+                # begin reading the frame from the server
+                while num > 0: 
+                    dump += client_socket.recv(min(100000, num))
+                    num -= min(100000, num)
+                # deserialize the frame
+                frame = pickle.loads(zlib.decompress(dump))
+                # display the frame
+                cv2.imshow("screen", frame)
+                # checks if ESC is pressed. If so, then the screen sharing window will close
+                if cv2.waitKey(1) == 27:
+                    break
+            except:
+                continue
+    except KeyboardInterrupt:
+        pass
+    # close connection
+    client_socket.close()
--- src/server.py
+++ src/server.py
@@ -1,3 +1,5 @@
+import os
+from multiprocessing import Process
 import numpy as np
 import cv2
 import pickle
@@ -5,40 +7,46 @@ import zlib
 from PIL import ImageGrab
 from socket import *
 
-# create server socket
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind(('', 12345))
+display_stream = 0 
 
-# initiate server's ability to listen
-server_socket.listen()
+if __name__=="__main__":
 
-# function used to continuously send frames
-def send_frames(connection_socket):
+    display_stream = os.environ['DISPLAY']
+
+    # create server socket
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(('', 12345))
+
+    # initiate server's ability to listen
+    server_socket.listen()
+
+    # function used to continuously send frames
+    def send_frames(connection_socket):
+        try:
+            while True:
+                # take a screenshot of current screen
+                img = ImageGrab.grab() # note the screenshot is stored as a PIL image
+                # converts screenshot into a byte array
+                img_np = np.array(img)
+                # PIL images are color as BGR, converting the color to RGB
+                frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+                # serialize the frame
+                data = zlib.compress(pickle.dumps(frame))
+                # send the frame size to client
+                connection_socket.sendall((str(len(data)) + "\n").encode())
+                # send the serialized frame to client
+                connection_socket.sendall(data)
+        except:
+            return "ok"
     try:
+        # loop to continousoly accept connections
         while True:
-            # take a screenshot of current screen
-            img = ImageGrab.grab() # note the screenshot is stored as a PIL image
-            # converts screenshot into a byte array
-            img_np = np.array(img)
-            # PIL images are color as BGR, converting the color to RGB
-            frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
-            # serialize the frame
-            data = zlib.compress(pickle.dumps(frame))
-            # send the frame size to client
-            connection_socket.sendall((str(len(data)) + "\n").encode())
-            # send the serialized frame to client
-            connection_socket.sendall(data)
-    except:
-        return "ok"
-try:
-    # loop to continousoly accept connections
-    while True:
-        # accept connection
-        connection_socket, addr = server_socket.accept()
-        # call outer func to send frames
-        ret = send_frames(connection_socket)
-            
-    # close server
-except KeyboardInterrupt:
-    pass
-server_socket.close()
+            # accept connection
+            connection_socket, addr = server_socket.accept()
+            # call outer func to send frames
+            ret = send_frames(connection_socket)
+
+        # close server
+    except KeyboardInterrupt:
+        pass
+    server_socket.close()
