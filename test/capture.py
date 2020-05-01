9caec8cf3fd3da0500bef576e395d836d80619ec
Devansh Panirwala <dpanirwa@purdue.edu>
2020-05-01 05:40:06 -0400

    adding telemetrics for UDP

--- test/capture.py
+++ test/capture.py
@@ -1,14 +1,55 @@
-from PIL import ImageGrab
-import numpy as np
-import cv2
-import tkinter as tk
+import time
 
-root = tk.Tk()
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
-img = ImageGrab.grab(bbox=(0,0,width,height)) #bbox specifies specific region (bbox= x,y,width,height *starts top-left)
-img_np = np.array(img) #this is the array obtained from conversion
-frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-cv2.imshow("test", frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import cv2
+import mss
+import numpy
+
+
+def screen_record():
+    try:
+        from PIL import ImageGrab
+    except ImportError:
+        return 0
+
+    # 800x600 windowed mode
+    mon = (0, 40, 800, 640)
+
+    title = "[PIL.ImageGrab] FPS benchmark"
+    fps = 0
+    last_time = time.time()
+
+    while time.time() - last_time < 1:
+        img = numpy.asarray(ImageGrab.grab(bbox=mon))
+        fps += 1
+
+        cv2.imshow(title, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
+
+    return fps
+
+
+def screen_record_efficient():
+    # 800x600 windowed mode
+    mon = {"top": 40, "left": 0, "width": 800, "height": 640}
+
+    title = "[MSS] FPS benchmark"
+    fps = 0
+    sct = mss.mss()
+    last_time = time.time()
+
+    while time.time() - last_time < 1:
+        img = numpy.asarray(sct.grab(mon))
+        fps += 1
+
+        cv2.imshow(title, img)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
+
+    return fps
+
+
+print("PIL:", screen_record())
+print("MSS:", screen_record_efficient())
