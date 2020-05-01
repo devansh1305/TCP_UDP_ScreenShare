a9ae471fdd995945be1434bda10ad0748433939b
Devansh Panirwala <dpanirwa@purdue.edu>
2020-05-01 05:33:05 -0400

    file name change

--- src/UDPClient.py
+++ src/UDPClient.py
@@ -1,95 +1,48 @@
+from __future__ import division
+import cv2
+import numpy as np
 import socket
 import struct
 import sys
-import time
-import mss
-import mss.tools
-import zlib
-import pickle
-import numpy
-import cv2
 
-# import tkinter as tk
-# root = tk.Tk()
-# WIDTH = root.winfo_screenwidth()
-# HEIGHT = root.winfo_screenheight()
+IMG_DATA_SIZE_MAX = 65536
 
-# monitor = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
-
-# frame = mss.mss().grab(monitor)
-
-# cframe = zlib.compress(pickle.dumps(frame))
-
-
-# data = numpy.array(frame)
-# print(len(zlib.compress(pickle.dumps(data), 9)))
-
-# data = cv2.resize(data, (int(WIDTH * 0.75), int(HEIGHT * 0.75)))
-# print(len(zlib.compress(pickle.dumps(data), 9)))
-
-# cv2.imshow('image', data)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
-message = b''
-for x in range(8192):
-    message += b'a'
-multicast_group = ('224.3.29.71', 10000)
-
-# Create the datagram socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-sock.settimeout(0.2)
-
-# Set the time-to-live for messages to 1 so they do not
-# go past the local network segment.
-ttl = struct.pack('b', 1)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
-count = 0
-ack = b'ok'
-try:
-    # Look for responses from all recipients
-    sent = sock.sendto()
+def align_buffer(my_client):
+    # Receiving all pizels
     while True:
-        
-        data = []
-        count = 0
-        temp = b''
-        for x in range(len(message)):
-            if count == 500:
-                data.append(temp)
-                temp = message[x]
-                continue
-            temp += message[x]
-        try:
-
-            while True:
-                for d in data:
-                    d.
-                
-        except:
-            continue
-
-        # Send data to the multicast group
-        print('sending {!r} - {}'.format("message", count))
-        sent = sock.sendto(str(len(message)).encode(), multicast_group)
-        sent = sock.sendto(message, multicast_group)
-        count += 1
-        print('waiting to receive')
-        try:
-            data, server = sock.recvfrom(16)
-        except socket.timeout:
-            print('timed out, no more responses')
+        segment, addr = my_client.recvfrom(IMG_DATA_SIZE_MAX)
+        print(segment[0])
+        if struct.unpack("B", segment[0:1])[0] == 1:
+            print("finish emptying buffer")
             break
-        else:
-            print('received {!r} from {}'.format(
-                data, server))
-        time.sleep(0.5)
 
-finally:
-    print('closing socket')
-    sock.close()
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 3:
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+    else:
+        host = "127.0.0.1"
+        port = 12345
+
+    # Set up socket
+    my_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_client.bind((host, port))
+    data = b''
+    align_buffer(my_client)
+
+    while True:
+        segment, addr = my_client.recvfrom(IMG_DATA_SIZE_MAX)
+        if struct.unpack("B", segment[0:1])[0] > 1:
+            data += segment[1:]
+        else:
+            data += segment[1:]
+            img = cv2.imdecode(np.fromstring(data, dtype=np.uint8), 1)
+            # img = np.frombuffer(data, dtype=np.uint8)
+            cv2.imshow('frame', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            data = b''
+
+    cv2.destroyAllWindows()
+    my_client.close()
--- src/UDPServer.py
+++ src/UDPServer.py
@@ -1,64 +1,72 @@
+from __future__ import division
+import numpy as np
+import sys
 import socket
 import struct
-import sys
-import time
+import math
 import mss
-import mss.tools
-import zlib
-import pickle
-import numpy
-import cv2
-
-# import tkinter as tk
-# root = tk.Tk()
-# WIDTH = root.winfo_screenwidth()
-# HEIGHT = root.winfo_screenheight()
-
-# monitor = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
-
-# frame = mss.mss().grab(monitor)
-
-# cframe = zlib.compress(pickle.dumps(frame))
+import cv2 as cv
+import tkinter as tk
+from PIL import ImageGrab
 
 
-# data = numpy.array(frame)
 
-# data = cv2.resize(data, (int(WIDTH * 0.75), int(HEIGHT * 0.75)))
-# print(len(zlib.compress(pickle.dumps(data), 6)))
+# Class to fragment image frame into multiple segments
+class FrameSegment(object):
+    IMG_DATA_SIZE_MAX = 65472
+    def __init__(self, sock, port = 12345, addr="127.0.0.1"):
+        self.my_server = sock
+        self.port = port
+        self.addr = addr
+
+    # Send the fragmented frames
+    def udp_frame(self, img):
+        # Compress image and Break down
+        # into data segments 
+        img_compress = cv.imencode('.jpg', img)[1]
+        data = img_compress.tostring()
+        size_of_img = len(data)
+        count = math.ceil(size_of_img/(self.IMG_DATA_SIZE_MAX))
+        buf_sent = 0
+        while count:
+            array_pos_end = min(size_of_img, buf_sent + self.IMG_DATA_SIZE_MAX)
+            self.my_server.sendto(struct.pack("B", count) + data[buf_sent:array_pos_end], (self.addr, self.port))
+            buf_sent = array_pos_end
+            count -= 1
 
 
-# multicast_group = '224.3.29.71'
-# server_address = ('', 10000)
+if __name__ == "__main__":
+    """ Top level main function """
+    # Set up UDP socket
+    width = 1000
+    height = 600
+    # root = tk.Tk()
+    # width = root.winfo_screenwidth()
+    # height = root.winfo_screenheight()
+    my_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    host = ""
+    port = ""
+    if len(sys.argv) == 3:
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+    else:
+        host = "127.0.0.1"
+        port = 12345
 
-# Create the socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    fs = FrameSegment(my_server, port, host)
 
-# Bind to the server address
-sock.bind(server_address)
-
-# Tell the operating system to add the socket to
-# the multicast group on all interfaces.
-group = socket.inet_aton(multicast_group)
-mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-sock.setsockopt(
-    socket.IPPROTO_IP,
-    socket.IP_ADD_MEMBERSHIP,
-    mreq)
-
-# Receive/respond loop
-while True:
-    print('\nwaiting to receive message')
-    num = ''
-    data, address = sock.recvfrom(1024)
-    print(data.decode())
-    # while data.decode() != '\n':
-    #     print(data.decode())
-    #     num += data.decode()
-    #     data, address = sock.recvfrom(1)
-    data, address = sock.recvfrom(int(data.decode()))
-    print('received {} bytes from {}'.format(
-        len(data), address))
-    print(data)
-
-    print('sending acknowledgement to', address)
-    sock.sendto(b'ack', address)
+    with mss.mss() as sct:
+        while True:
+            # Get the screen capture
+            # bbox specifies specific region (bbox= x,y,width,height *starts top-left)
+            monitor = {"top": 0, "left": 0, "width": width, "height": height}
+            frame = np.array(sct.grab(monitor))
+            #frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            
+            # screenshot = mss.mss().grab(monitor) 
+            # img = Image.frombytes("RGB", (screenshot.width, screenshot.height), screenshot.rgb)
+            # frame = numpy.array(mss.mss().grab(monitor))
+            fs.udp_frame(frame)
+    cv.destroyAllWindows()
+    my_server.close()
