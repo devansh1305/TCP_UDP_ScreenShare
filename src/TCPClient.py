2015fba0421f64e0abf3a2cfd8d83154d64a6009
kevinyin1 <kevin.yin3@gmail.com>
2020-04-30 21:07:30 -0400

    finish TCP screen share implementation

--- src/TCPClient.py
+++ src/TCPClient.py
@@ -29,53 +29,65 @@ Libraries/Modules used
 
 '''
 
-if len(sys.argv) < 2:
-    hostname = "DESKTOP-QHVEDFE"
-    port = 12345
-else:
-    hostname = "localhost"
-    port = sys.argv[1]
-    
-root = tk.Tk()
-WIDTH = root.winfo_screenwidth()
-HEIGHT = root.winfo_screenheight()
+class TCPClient():
 
-# create client socket
-client_socket = socket(AF_INET, SOCK_STREAM)
-# connect client socket to localhost with specified port
-host = gethostbyname(hostname)
-client_socket.connect((host, port))
-# name a window and adjust it's size
-cv2.namedWindow("screen", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("screen", int(WIDTH * 0.75), int(HEIGHT * 0.75))
-try:
-    # continuous loop that gets frames from the server
-    while True:
+    def __init__(self, hostname, port, width = 720, height = 480):
+        # create client socket
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        # connect client socket to localhost with specified port
+        self.client_socket.connect((gethostbyname(hostname), port))
+        self.screen_width = width
+        self.screen_height = height
+    
+    def set_full_screen(self):
+        root = tk.Tk()
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+
+    def resize_window(self):
+        cv2.namedWindow("screen", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("screen", self.screen_width, self.screen_height)
+
+    def run(self):
+        # self.set_full_screen()
+        self.resize_window()
+
         try:
-            last_time = time.time()
-            num = "" # used to get the number of frame bytes to read from the server
-            data = client_socket.recv(1).decode()
-            dump = b''
-            while data != "\n": # continue reading the frame size until new line
-                num += data
-                data = client_socket.recv(1).decode()
-            # convert frame size num into an int
-            num = int(num)
-            # begin reading the frame from the server
-            while num > 0: 
-                dump += client_socket.recv(min(100000, num))
-                num -= min(100000, num)
-            # deserialize the frame
-            frame = pickle.loads(zlib.decompress(dump))
-            # display the frame
-            cv2.imshow("screen", frame)
-            print("fps: {}".format(1 / (time.time() - last_time)))
-            # checks if ESC is pressed. If so, then the screen sharing window will close
-            if cv2.waitKey(1) == 27:
-                break
-        except:
-            continue
-except KeyboardInterrupt:
-    pass
-# close connection
-client_socket.close()
+            # continuous loop that gets frames from the server
+            while True:
+                try:
+                    last_time = time.time()
+                    num = "" # used to get the number of frame bytes to read from the server
+                    data = self.client_socket.recv(1).decode()
+                    dump = b''
+                    while data != "\n": # continue reading the frame size until new line
+                        num += data
+                        data = self.client_socket.recv(1).decode()
+                    # convert frame size num into an int
+                    num = int(num)
+                    # begin reading the frame from the server
+                    while num > 0: 
+                        dump += self.client_socket.recv(min(100000, num))
+                        num -= min(100000, num)
+                    # deserialize the frame
+                    frame = pickle.loads(zlib.decompress(dump))
+                    # display the frame
+                    cv2.imshow("screen", frame)
+                    print("fps: {}".format(1 / (time.time() - last_time)))
+                    # checks if ESC is pressed. If so, then the screen sharing window will close
+                    if cv2.waitKey(1) == 27:
+                        break
+                except:
+                    continue
+        finally:
+            print("disconnected")
+            self.client_socket.close()
+
+if len(sys.argv) == 3:
+    hostname = sys.argv[1]
+    port = int(sys.argv[2])
+    tcp_client = TCPClient(hostname, port)
+    tcp_client.run()
+else:
+    print("invalid arguments")
+    sys.exit(1)
--- src/TCPServer.py
+++ src/TCPServer.py
@@ -1,5 +1,5 @@
 from socket import *
-from threading import Thread
+import threading
 import sys
 import numpy
 import cv2
@@ -7,125 +7,108 @@ import pickle
 import zlib
 import mss
 
-TCP = 0
-UDP = 1
+LIST = 0
+THREAD = 1
 
 import tkinter as tk
 
 class TCPServer():
 
-    def __init__(self, process_type):
+    def __init__(self, port, process_type, width = 720, height = 480, compression_level = 6):
         self.clients = []
         self.type = process_type
+        self.screen_width = width
+        self.screen_height = height
+        self.compression_level = compression_level
+        self.monitor = {"top": 0, "left": 0, "width": self.screen_width, "height": self.screen_height}
 
-    def 
-
-
-'''
-class TCPserver:
-    def __init__(self,clients,process_type):
-        self.clients = clients
-        self.ptype = process_type
+        self.server_socket = socket(AF_INET, SOCK_STREAM)
+        self.server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.server_socket.bind(('', port))
+        self.server_socket.listen()
 
     def send_frames_list(self):
         try:
-            # loop to continousoly accept connections
             while True:
-                #print("inside fr")
-                # take a screenshot of current screen
-                frame = numpy.array(mss.mss().grab(monitor))
-                # serialize the frame
-                data = zlib.compress(pickle.dumps(cv2.resize(frame, (int(WIDTH * 0.75), int(HEIGHT * 0.75)))), compression_level)
-                for client in clients:
+                screenshot = mss.mss().grab(self.monitor)
+                pixel_array = numpy.array(screenshot)
+                serialized_parray = pickle.dumps(pixel_array)
+                compressed_sparray = zlib.compress(serialized_parray, self.compression_level)
+                size = len(compressed_sparray)
+                for client in self.clients:
                     try:
                         # send the frame size to client
-                        client.sendall((str(len(data)) + "\n").encode())
+                        client.sendall((str(size) + "\n").encode())
                         # send the serialized frame to client
-                        client.sendall(data)
+                        client.sendall(compressed_sparray)
                     except:
-                        clients.remove(client)
+                        self.clients.remove(client)
                         print("removed a client from list of clients")
-                        if(len(clients)==0): 
-                            print("last client removed")
-                            continue
-        except:
+        finally:
+            for client in self.clients:
+                client.close()
             return
 
+    def send_frames_threading(self, connection_socket):
+        try:
+            while True:
+                screenshot = mss.mss().grab(self.monitor)
+                pixel_array = numpy.array(screenshot)
+                serialized_parray = pickle.dumps(pixel_array)
+                compressed_sparray = zlib.compress(serialized_parray, self.compression_level)
+                size = len(compressed_sparray)
+                connection_socket.sendall((str(size) + "\n").encode())
+                connection_socket.sendall(compressed_sparray)
+        finally:
+            connection_socket.close()
+            print("Thread: a client has disconnected")
+            return
+
+    def get_full_screen(self):
+        root = tk.Tk()
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+        self.montior = {"top": 0, "left": 0, "width": self.screen_width, "height": self.screen_height}
+        return (self.screen_width, self.screen_height)
+
     def run(self):
-        if self.ptype=="list":
+        print("Server has started...")
+        # self.get_full_screen()
+        if self.type == LIST:
+            t = threading.Thread(target = self.send_frames_list, args = ()).start()
             try:
                 while True:
-                    connection_socket, addr = server_socket.accept()
-                    clients.append(connection_socket)
-                    print("adding client to list of clients")
-                    t = Thread(target=self.send_frames_list, args=()).start()
-                    print("started thread...")
-            except:
-                print("socket closed")
-                server_socket.close()
-                #sys.exit(0)
-
-
-
-if __name__ == "__main__":
-    clients = []
-    root = tk.Tk()
-    WIDTH = root.winfo_screenwidth()
-    HEIGHT = root.winfo_screenheight()
-    monitor = {"top": 0, "left": 0, "width": WIDTH, "height": HEIGHT}
-    if len(sys.argv) < 2:
-        connection_type = TCP
-        port = 12345
-        compression_level = 2   
-    else:
-        connection_type = sys.argv[1]
-        port = sys.argv[2]
-        compression_level = sys.argv[3]
-
-    # create server socket
-    server_socket = socket(AF_INET, SOCK_STREAM)
-    server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    server_socket.bind(('', port))
-    # initiate server's ability to listen
-    server_socket.listen()
-    a = TCPserver(clients,process_type="list")
-    a.run()
-
-
-# function used to continuously send frames
-
-def send_frames():
-    try:
-        # loop to continousoly accept connections
-        while True:
-            # take a screenshot of current screen
-            frame = numpy.array(mss.mss().grab(monitor))
-            # serialize the frame
-            data = zlib.compress(pickle.dumps(frame), compression_level)
-            for client in clients:
-                try:
-                    # send the frame size to client
-                    client.sendall((str(len(data)) + "\n").encode())
-                    # send the serialized frame to client
-                    client.sendall(data)
-                except:
-                    clients.remove(client)
-                    print("removed a client from list of clients")
-    except:
-        return
-
-try:
-    # loop to continousoly accept connections
-    while True:
-        # accept connection
-        connection_socket, addr = server_socket.accept()
-        # call outer func to send frames
-        clients.append(connection_socket)
-        print("adding client to list of clients")
+                    connection_socket, addr = self.server_socket.accept()
+                    self.clients.append(connection_socket)
+                    print("List: adding client to list of clients")
+            finally:
+                self.server_socket.close()
+                return
+        else:
+            try:
+                while True:
+                    connection_socket, addr = self.server_socket.accept()
+                    threading.Thread(target = self.send_frames_threading, args = (connection_socket,)).start()
+                    print("Thread: a client has connected")
+            finally:
+                self.server_socket.close()
+                return
             
-    # close server
-#
-except:
-    server_socket.close()
-    sys.exit(0)
-'''
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        tcp_server = TCPServer(12345, LIST)
+        tcp_server.run()
+    elif len(sys.argv) == 2:
+        tcp_server = TCPServer(int(sys.argv[1]), LIST)
+        tcp_server.run()
+    elif len(sys.argv) == 3:
+        process_type = LIST if (sys.argv[2] == "LIST") else THREAD
+        tcp_server = TCPServer(int(sys.argv[1]), process_type)
+        tcp_server.run()
+    elif len(sys.argv) == 4:
+        process_type = LIST if (sys.argv[2] == "LIST") else THREAD
+        tcp_server = TCPServer(int(sys.argv[1]), process_type, 720, 480, int(sys.argv[3]))
+        tcp_server.run()
+    else:
+        print("Invalid Arguments")
+        sys.exit(1)
